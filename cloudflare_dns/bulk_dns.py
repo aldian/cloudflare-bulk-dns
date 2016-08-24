46cfedb55b6d7940dcf9d67ace3bf41312e83643
aldian <mobile@aldian.net>
2016-08-24 15:52:50 +0800

    update import path

--- cloudflare_dns/bulk_dns.py
+++ cloudflare_dns/bulk_dns.py
@@ -3,6 +3,7 @@ import os
 import sys
 from functools import wraps
 from CloudFlare.exceptions import CloudFlareAPIError
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from cloudflare_dns import CloudFlareLibWrapper
 
 
