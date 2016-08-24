85942fdbd31215cd756a1484b37949fbafcffdef
aldian <mobile@aldian.net>
2016-08-24 15:22:10 +0800

    implement CLI for adding new domain

--- cloudflare_dns/bulk_dns.py
+++ cloudflare_dns/bulk_dns.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import sys
 from functools import wraps
@@ -22,23 +23,39 @@ def configured(f):
     return decorated_function
 
 
+def add_new_domain(domain_name, domain_added_cb=None, cf_lib_wrapper=None):
+    try:
+        zone_info = cf_lib_wrapper.create_a_zone(domain_name)
+        if domain_added_cb is not None and hasattr(domain_added_cb, '__call__'):
+            domain_added_cb(succeed=True, response=zone_info)
+    except CloudFlareAPIError as e:
+        if "already exists" not in e.message:
+            raise e
+        if domain_added_cb is not None and hasattr(domain_added_cb, '__call__'):
+            domain_added_cb(succeed=False, exception=e)
+
+
 @configured
 def add_new_domains(domain_names, domain_added_cb=None, cf_lib_wrapper=None):
     for domain_name in domain_names:
-        try:
-            zone_info = cf_lib_wrapper.create_a_zone(domain_name)
-            if domain_added_cb is not None and hasattr(domain_added_cb, '__call__'):
-                domain_added_cb(succeed=True, response=zone_info)
-        except CloudFlareAPIError as e:
-            if "already exists" not in e.message:
-                raise e
-            if domain_added_cb is not None and hasattr(domain_added_cb, '__call__'):
-                domain_added_cb(succeed=False, exception=e)
+        add_new_domain(domain_name, domain_added_cb=domain_added_cb, cf_lib_wrapper=cf_lib_wrapper)
 
 
 @configured
 def cli(args, cf_lib_wrapper=None):
-    pass
+    counter = 0
+
+    def domain_added_cb(succeed=None, response=None, exception=None):
+        if succeed:
+            print("added [{0}]: {1}".format(counter + 1, response['name']))
+        else:
+            print("failed [{0}]: {1}".format(counter + 1, exception.message))
+
+    if args[0] == '--add-new-domains':
+        with open(args[1]) as f:
+            for line in f:
+                add_new_domain(line.strip(), domain_added_cb=domain_added_cb, cf_lib_wrapper=cf_lib_wrapper)
+                counter += 1
 
 if __name__ == "__main__":
     cli(sys.argv[1:])
