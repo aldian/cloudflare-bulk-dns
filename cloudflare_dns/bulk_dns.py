20b63f7c89ba240bd9476b73b10596dcaedf7145
Aldian Fazrihady <mobile@aldian.net>
2016-08-25 01:56:18 +0700

    writing report to csv file

--- cloudflare_dns/bulk_dns.py
+++ cloudflare_dns/bulk_dns.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import os
 import sys
+import datetime
+import csv
 from functools import wraps
 from CloudFlare.exceptions import CloudFlareAPIError
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -39,18 +41,29 @@ def add_new_domain(domain_name, domain_added_cb=None, cf_lib_wrapper=None):
 @configured
 def cli(args, cf_lib_wrapper=None):
     counter = 0
-
-    def domain_added_cb(succeed=None, response=None, exception=None):
-        if succeed:
-            print("added [{0}]: {1}".format(counter + 1, response['name']))
-        else:
-            print("failed [{0}]: {1}".format(counter + 1, exception.message))
+    dt = datetime.datetime.now()
 
     if args[0] == '--add-new-domains':
-        with open(args[1]) as f:
-            for line in f:
-                add_new_domain(line.strip(), domain_added_cb=domain_added_cb, cf_lib_wrapper=cf_lib_wrapper)
-                counter += 1
+        with open("cf_dns_add_new_domains_{0:04}{1:02}{2:02}_{3:02}{4:02}{5:02}.csv".format(
+                dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second), "wb") as csv_file:
+            writer = csv.writer(csv_file)
+
+            def domain_added_cb(succeed=None, response=None, exception=None):
+                output_text = ''
+                if succeed:
+                    output_text = "added [{0}]: {1}".format(counter + 1, response['name'])
+                else:
+                    output_text = "failed [{0}]: {1}".format(counter + 1, exception.message)
+                print(output_text)
+                writer.writerow([output_text])
+
+            domains_file_name = args[1]
+            with open(domains_file_name) as f:
+                print("Adding domains listed in {0}:".format(domains_file_name))
+                for line in f:
+                    add_new_domain(line.strip(), domain_added_cb=domain_added_cb, cf_lib_wrapper=cf_lib_wrapper)
+                    counter += 1
+                print("Added {0} new domains.".format(counter))
 
 if __name__ == "__main__":
     cli(sys.argv[1:])
