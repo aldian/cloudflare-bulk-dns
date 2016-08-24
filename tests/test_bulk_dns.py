586b3df584ab91768ffe8efe872b185c9237c548
aldian <mobile@aldian.net>
2016-08-24 13:04:17 +0800

    CLI for adding new domains

--- tests/test_bulk_dns.py
+++ tests/test_bulk_dns.py
@@ -16,12 +16,29 @@ class TestBulkDns(unittest.TestCase):
         pass
 
     def test_add_new_domains(self):
+        responses = []
+
+        def domain_added_cb(**kwargs):
+            self.assertTrue(kwargs['succeed'])
+            response = kwargs['response']
+            responses.append(response)
+            self.assertEqual('ZONE INFO ID', response['id'])
+
         self.cf_lib_wrapper.create_a_zone = MagicMock(return_value={'id': 'ZONE INFO ID'})
         domain_names = ['add-purer-happen.host', 'analyze-dry.win', 'juiciest-old-flap.tech']
-        bulk_dns.add_new_domains(domain_names, domain_added_cb=None, cf_lib_wrapper=self.cf_lib_wrapper)
+        bulk_dns.add_new_domains(
+            domain_names, domain_added_cb=domain_added_cb,
+            cf_lib_wrapper=self.cf_lib_wrapper)
+        self.assertEqual(3, len(responses))
 
     def test_cli_add_new_domains(self):
-        pass
+
+        def add_new_domains_mock(bulk_dns):
+            pass
+
+        bulk_dns.add_new_domains = add_new_domains_mock
+        self.cf_lib_wrapper.create_a_zone = MagicMock(return_value={'id': 'ZONE INFO ID'})
+        bulk_dns.cli(['--add-new-domains', 'example-domains.txt'], cf_lib_wrapper=self.cf_lib_wrapper)
 
 if __name__ == '__main__':
     unittest.main()
